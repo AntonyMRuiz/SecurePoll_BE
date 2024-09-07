74e0c0713a5165262ba538d07e74833a3ce5161c
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-07 18:43:04 -0500

    The "DocumentType" model was created

---
+++ Controllers/DocumentType.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SecurePoll_BE.Controllers;
+
+[Table("document_types")]
+public class DocumentType
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Column("name")]
+    public required string Name { get; set; }
+
+    [Column("abbreviation")]
+    public required string Abbreviation { get; set; }
+}
