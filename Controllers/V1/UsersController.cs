11446b832bf074297bf98e6e3269ce8fe57ff94e
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-08 00:42:31 -0500

    User get methods have been improved

--- Controllers/V1/UsersController.cs
+++ Controllers/V1/UsersController.cs
@@ -20,14 +20,14 @@ namespace SecurePoll_BE.Controllers.V1
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return await _context.Users.Include(p=>p.DocumentType).Include(p=>p.Role).ToListAsync();
         }
 
         // GET: api/Users/5
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var user = await _context.Users.Include(p=>p.DocumentType).Include(p=>p.Role).FirstOrDefaultAsync(p => p.Id == id);
 
             if (user == null)
             {
