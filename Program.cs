441d62d1c6f3ea6dd28b8a75eeb74762abb49889
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-08 01:22:23 -0500

    the program was improved

--- Program.cs
+++ Program.cs
@@ -15,6 +15,11 @@ var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
 var conectionDB = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
 
+// Development
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+                    ServerVersion.Parse("8.0.20-mysql")));
+// Production
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(conectionDB, ServerVersion.Parse("8.0.20-mysql")));
 
