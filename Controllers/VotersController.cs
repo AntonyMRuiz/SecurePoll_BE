bff1a3e43e55f3d655271388fb4c431e78007572
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-07 22:52:45 -0500

    The "Voters" controller was created

---
+++ Controllers/VotersController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecurePoll_BE.Data;
+using SecurePoll_BE.Models;
+
+namespace SecurePoll_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VotersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VotersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Voters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Voter>>> GetVoters()
+        {
+            return await _context.Voters.ToListAsync();
+        }
+
+        // GET: api/Voters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Voter>> GetVoter(int id)
+        {
+            var voter = await _context.Voters.FindAsync(id);
+
+            if (voter == null)
+            {
+                return NotFound();
+            }
+
+            return voter;
+        }
+
+        // PUT: api/Voters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVoter(int id, Voter voter)
+        {
+            if (id != voter.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(voter).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VoterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Voters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Voter>> PostVoter(Voter voter)
+        {
+            _context.Voters.Add(voter);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVoter", new { id = voter.Id }, voter);
+        }
+
+        // DELETE: api/Voters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVoter(int id)
+        {
+            var voter = await _context.Voters.FindAsync(id);
+            if (voter == null)
+            {
+                return NotFound();
+            }
+
+            _context.Voters.Remove(voter);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VoterExists(int id)
+        {
+            return _context.Voters.Any(e => e.Id == id);
+        }
+    }
+}
